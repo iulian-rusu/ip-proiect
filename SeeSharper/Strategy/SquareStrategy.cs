51149d50190d39eb7ef4f7efc04b20ae031242db
Iulian <iulian.rusu999@gmail.com>
2021-04-26 22:01:32 +0300

    Modify Circle and Square Strategies

--- SeeSharper/Strategy/SquareStrategy.cs
+++ SeeSharper/Strategy/SquareStrategy.cs
@@ -11,10 +11,18 @@ namespace Strategy
             if (_points != null)
             {
                 var graphics = e.Graphics;
-                int width = Math.Abs(_points[1].X - _points[0].X);
-                int startX = Math.Min(_points[1].X, _points[0].X);
-                int startY = Math.Min(_points[1].Y, _points[0].Y);
-                var rect = new Rectangle(startX, startY, width, width);
+
+                int midX = _points[0].X;
+                int midY = _points[0].Y;
+
+                int width = Math.Abs(_points[1].X - midX);
+                int height = Math.Abs(_points[1].Y - midY);
+                int dim = Math.Max(width, height);
+
+                int startX = midX - dim;
+                int startY = midY - dim;
+
+                var rect = new Rectangle(startX, startY, 2 * dim, 2 * dim);
                 graphics.DrawRectangle(new Pen(_color), rect);
             }
         }
