4b85fc137c2f8ba4cefe2ce44770a9ac631a44c3
Alina Nistor <alina.nistor99@gmail.com>
2021-05-03 20:49:18 +0300

    Add arrow right draw method

--- SeeSharper/Strategy/ArrowRightStrategy.cs
+++ SeeSharper/Strategy/ArrowRightStrategy.cs
@@ -15,6 +15,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -26,12 +27,51 @@ namespace Strategy
     {
         public override string GetDescription()
         {
-            throw new NotImplementedException();
+            if (!_hasDrawn)
+            {
+                return "Nothing drawn";
+            }
+
+            if (_points != null)
+            {
+                return $"Draw arrow right with corner ({_points[0].X}, {_points[0].Y}) and ({_points[1].X}, {_points[1].Y})";
+            }
+            return "Something wrong";
         }
 
         protected override void Draw(object sender, PaintEventArgs e)
         {
-            throw new NotImplementedException();
+            if (_points != null)
+            {
+                var graphics = e.Graphics;
+                Point[] arrowPoints = new Point[7];
+
+                int bodyWidth = Math.Abs((int)((_points[1].X - _points[0].X) / 2.0));
+                int bodyHeight = (int)((2.0 / 3.0) * (_points[1].Y - _points[0].Y));
+                int arrowheadMargin = (int)(bodyHeight / 2.0);
+
+                Point startPoint; // lower left corner
+
+                if (_points[1].X - _points[0].X > 0)
+                {
+                    startPoint = new Point(_points[0].X, _points[0].Y + bodyHeight);
+                }
+                else
+                {
+                    startPoint = new Point(_points[1].X, _points[1].Y - arrowheadMargin);
+                }
+
+                arrowPoints[0] = startPoint;
+                arrowPoints[1] = new Point(startPoint.X, startPoint.Y - bodyHeight);
+                arrowPoints[2] = new Point(startPoint.X + bodyWidth, startPoint.Y - bodyHeight);
+                arrowPoints[3] = new Point(startPoint.X + bodyWidth, startPoint.Y - bodyHeight - arrowheadMargin);
+                arrowPoints[4] = new Point(startPoint.X + 2 * bodyWidth, startPoint.Y - bodyHeight / 2);
+                arrowPoints[5] = new Point(startPoint.X + bodyWidth, startPoint.Y + arrowheadMargin);
+                arrowPoints[6] = new Point(startPoint.X + bodyWidth, startPoint.Y);
+
+                graphics.FillPolygon(new SolidBrush(_fillColor), arrowPoints);
+                graphics.DrawPolygon(new Pen(_color, _thickness), arrowPoints);
+            }
         }
     }
 }
