c71900d37331cbb5f8aca6e768f39071aa955f6d
Alina Nistor <alina.nistor99@gmail.com>
2021-05-04 17:51:35 +0300

    Implement eraser

--- SeeSharper/Strategy/EraserStrategy.cs
+++ SeeSharper/Strategy/EraserStrategy.cs
@@ -15,6 +15,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,19 +25,65 @@ namespace Strategy
 {
     public class EraserStrategy : MultiplePointStrategy
     {
-        public override void MouseMoved(int x, int y)
-        {
-            throw new NotImplementedException();
-        }
-
+        List<Rectangle> _rectangles;
         public override void MouseStateChanged(int x, int y)
         {
-            throw new NotImplementedException();
+            if (_points == null)
+            {
+                _points = new List<Point>();
+                _rectangles = new List<Rectangle>();
+                _points.Add(new Point(x, y));
+                _rectangles.Add(new Rectangle(x - 3 * (int)_thickness / 2, y - 3 * (int)_thickness / 2, 3 * (int)_thickness, 3 * (int)_thickness));
+                _hasDrawn = true;
+            }
+            else
+            {
+                _done = true;
+            }
+        }
+
+        public override void MouseMoved(int x, int y)
+        {
+            if (_points != null && !_done)
+            {
+                _points.Add(new Point(x, y));
+                _rectangles.Add(new Rectangle(x - 3 * (int)_thickness / 2, y - 3 * (int)_thickness / 2, 3 * (int)_thickness, 3 * (int)_thickness));
+                _hasDrawn = true;
+            }
         }
 
         protected override void Draw(object sender, PaintEventArgs e)
         {
-            throw new NotImplementedException();
+            if (_points != null)
+            {
+                var graphics = e.Graphics;
+                for (int i = 0; i < _points.Count - 1; i++)
+                {
+                    Point[] points = new Point[4];
+                    int width = _points[i + 1].X - _points[i].X;
+                    int height = _points[i + 1].Y - _points[i].Y;
+                    int size = _rectangles[0].Width;
+
+                    if (width * height <= 0)
+                    {
+                        points[0] = new Point(_rectangles[i].X, _rectangles[i].Y);
+                        points[1] = new Point(_rectangles[i].X + size, _rectangles[i].Y + size);
+                        points[3] = new Point(_rectangles[i + 1].X, _rectangles[i + 1].Y);
+                        points[2] = new Point(_rectangles[i + 1].X + size, _rectangles[i + 1].Y + size);
+                    }
+                    else
+                    {
+                        points[0] = new Point(_rectangles[i].X + size, _rectangles[i].Y);
+                        points[1] = new Point(_rectangles[i].X, _rectangles[i].Y + size);
+                        points[2] = new Point(_rectangles[i + 1].X, _rectangles[i + 1].Y + size);
+                        points[3] = new Point(_rectangles[i + 1].X + size, _rectangles[i + 1].Y);
+                    }
+
+                    graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+                    graphics.FillRectangle(new SolidBrush(Color.White), _rectangles[i]);
+                    graphics.FillPolygon(new SolidBrush(Color.White), points);
+                }
+            }
         }
     }
 }
