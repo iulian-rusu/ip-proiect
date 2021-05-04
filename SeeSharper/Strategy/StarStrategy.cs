dd3f06c2bff6b41fdb3707697a14ed1afdfa253f
Alina Nistor <alina.nistor99@gmail.com>
2021-05-04 11:39:21 +0300

    Add star draw method

--- SeeSharper/Strategy/StarStrategy.cs
+++ SeeSharper/Strategy/StarStrategy.cs
@@ -16,6 +16,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -27,12 +28,52 @@ namespace Strategy
     {
         public override string GetDescription()
         {
-            throw new NotImplementedException();
+            if (!_hasDrawn)
+            {
+                return "Nothing drawn";
+            }
+
+            if (_points != null)
+            {
+                return $"Draw star with corner ({_points[0].X}, {_points[0].Y}) and ({_points[1].X}, {_points[1].Y})";
+            }
+            return "Something wrong";
         }
 
         protected override void Draw(object sender, PaintEventArgs e)
         {
-            throw new NotImplementedException();
+            if (_points != null)
+            {
+                var graphics = e.Graphics;
+
+                float width = _points[1].X - _points[0].X;
+                double ang36 = Math.PI / 5.0;
+                double ang72 = 2.0 * ang36;
+                float sin36 = (float)Math.Sin(ang36);
+                float sin72 = (float)Math.Sin(ang72);
+                float cos36 = (float)Math.Cos(ang36);
+                float cos72 = (float)Math.Cos(ang72);
+                float outerRadius = width;
+                float innerRadius = (float)(width / 2.0);
+
+                PointF center = _points[0];
+                PointF[] starPoints = new PointF[10];
+
+                starPoints[0] = new PointF(center.X, center.Y - outerRadius);
+                starPoints[1] = new PointF(center.X + innerRadius * sin36, center.Y - innerRadius * cos36);
+                starPoints[2] = new PointF(center.X + outerRadius * sin72, center.Y - outerRadius * cos72);
+                starPoints[3] = new PointF(center.X + innerRadius * sin72, center.Y + innerRadius * cos72);
+                starPoints[4] = new PointF(center.X + outerRadius * sin36, center.Y + outerRadius * cos36);
+                starPoints[5] = new PointF(center.X, center.Y + innerRadius);
+                starPoints[6] = new PointF(2 * center.X - starPoints[4].X, starPoints[4].Y);
+                starPoints[7] = new PointF(2 * center.X - starPoints[3].X, starPoints[3].Y);
+                starPoints[8] = new PointF(2 * center.X - starPoints[2].X, starPoints[2].Y);
+                starPoints[9] = new PointF(2 * center.X - starPoints[1].X, starPoints[1].Y);
+
+                graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+                graphics.FillPolygon(new SolidBrush(_fillColor), starPoints);
+                graphics.DrawPolygon(new Pen(_color, _thickness), starPoints);
+            }
         }
     }
 }
