3d863abab3e58347c05122270c2342e4d7692efb
xeno-john <ionut-alexandru.baltariu@student.tuiasi.ro>
2021-05-23 09:43:17 +0300

    Add Doxygen comments to IModel

--- SeeSharper/Shared/IModel.cs
+++ SeeSharper/Shared/IModel.cs
@@ -1,9 +1,29 @@
-﻿namespace Shared
+﻿/**************************************************************************
+ *                                                                        *
+ *  File:        IModel.cs                                                *
+ *  Copyright:   (c) 2021, Baltariu Ionut-Alexandru                       *
+ *  E-mail:      ionut-alexandru.baltariu@student.tuiasi.ro               *
+ *  Description: Interface for the Model class of the                     *
+ *               Model-View-Presenter architecture.c                      *
+ *                                                                        *
+ *  This code and information is provided "as is" without warranty of     *
+ *  any kind, either expressed or implied, including but not limited      *
+ *  to the implied warranties of merchantability or fitness for a         *
+ *  particular purpose. You are free to use this source code in your      *
+ *  applications as long as the original copyright notice is included.    *
+ *                                                                        *
+ **************************************************************************/
+
+namespace Shared
 {
     using Memento;
     using Strategy;
     using System.Drawing;
 
+    /// <summary>
+    /// Interface for the Model Class of the MVP architecture -
+    /// It contains the data, state and logic of the application.
+    /// </summary>
     public interface IModel
     {
         void SaveDrawing(DrawingMemento drawingMemento);
